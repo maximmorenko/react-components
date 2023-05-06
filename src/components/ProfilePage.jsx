1cb8e0b0fdf71938de6254a2bb7cb9ec87068599
Maksym <morenko.maxim@gmail.com>
2023-05-06 06:21:27 +0300

    fixed assets

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -7,8 +7,7 @@ function ProfilePage() {
       <NavBar />
       <h1>All About Me!</h1>
       <p>I like movies and blah blah blah blah blah</p>
-      <img src="https://github.com/maximmorenko/memory-game/blob/master/assets/img/js_back.jpg?raw=true" alt='my'/>
-      {/* <img src="src/assets/1.jpg" alt='my'/> */}
+      <img src="assets/cat.jpg" alt='cat'/>
     </div>
   );
 }
